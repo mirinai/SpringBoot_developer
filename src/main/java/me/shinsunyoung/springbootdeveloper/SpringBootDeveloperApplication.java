d25d022cd76dda3ed56d92cbf10655a5fcbbb849
mirinai <tmddjs1771@gmail.com>
2025-01-22 14:56:17 +0900

    test .idea

--- src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDeveloperApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
+
+
     }
 }
